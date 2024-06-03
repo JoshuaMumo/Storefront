69b2c44d10d239618aa3a4ce51d4d819d121b0c9
jmumo <jmumo6672@gmail.com>
2024-06-03 12:23:30 +0300

    added users

--- users/views.py
+++ users/views.py
@@ -1,16 +1,7 @@
-from django.shortcuts import render, redirect
-from django.contrib import messages
-from .forms import UserRegisterForm
+from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
 
 
 def register(request):
-    if request.method == 'POST':
-        form = UserRegisterForm(request.POST)
-        if form.is_valid():
-            form.save()
-            username = form.cleaned_data.get('username')
-            messages.success(request, f'Account created for {username}!')
-            return redirect('blog-home')
-    else:
-        form = UserRegisterForm()
-    return render(request, 'register.html', {'form': form})
+   form = UserCreationForm()
+   return render(request, 'register.html',{'form': form})
