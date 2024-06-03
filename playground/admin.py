b9c3fe6d636add55770b513654d80b58f9d73071
jmumo <jmumo6672@gmail.com>
2024-06-03 11:51:57 +0300

    subjects

--- playground/admin.py
+++ playground/admin.py
@@ -1,17 +1,26 @@
 from django.contrib import admin
-from .models import Student, Teacher
+from .models import Student, Teacher, Subject
 
 # Register your models here.
 admin.site.site_header = 'JOSHUA ADMIN'
 admin.site.register(Teacher)
+admin.site.register(Subject)
 
 class StudentAdminSite(admin.ModelAdmin):
     model = Student
-    fields = ['name', 'admno', 'grade','gender','age']
+    fields = ['name', 'admno', 'grade', 'subjects']
     list_display = ('name', 'admno', 'grade')
     actions = ['promote_to_next_grade']
 
     def promote_to_next_grade(self, request, queryset):
+        subjects = ['Maths', 'English', 'Kiswahili', 'French', 'Computer Studies']
+        for subject_name in subjects:
+            Subject.objects.get_or_create(name=subject_name)
+        
+        grade_1_to_3_subjects = Subject.objects.filter(name__in=['Maths', 'English', 'Kiswahili'])
+        grade_4_subjects = Subject.objects.filter(name__in=['Maths', 'English', 'Kiswahili', 'French'])
+        grade_5_to_6_subjects = Subject.objects.filter(name__in=['Maths', 'English', 'Kiswahili', 'French', 'Computer Studies'])
+
         for student in queryset:
             current_grade = student.grade
             if current_grade.isdigit():
@@ -21,9 +30,21 @@ class StudentAdminSite(admin.ModelAdmin):
                 else:
                     student.grade = str(current_grade_int + 1)
                     student.save()
+
+                    # Assign subjects based on the new grade
+                    if current_grade_int + 1 <= 3:
+                        student.subjects.set(grade_1_to_3_subjects)
+                    elif current_grade_int + 1 == 4:
+                        student.subjects.set(grade_4_subjects)
+                    elif 5 <= current_grade_int + 1 <= 6:
+                        student.subjects.set(grade_5_to_6_subjects)
+                    student.save()
             else:
+                # Handle non-numeric grade logic, if applicable
                 pass
-        self.message_user(request, "Selected students have been promoted to the next grade or removed if they finished grade 6.")
+        self.message_user(request, "Selected students have been promoted to the next grade, assigned appropriate subjects, or removed if they finished grade 6.")
     
-    promote_to_next_grade.short_description = "Promote selected students to the next grade or remove if they finished grade 6"
+    promote_to_next_grade.short_description = "Promote selected students to the next grade, assign subjects, or remove if they finished grade 6"
+
+# Register the Student model with the StudentAdminSite
 admin.site.register(Student, StudentAdminSite)
--- playground/models.py
+++ playground/models.py
@@ -3,12 +3,19 @@ from .choices import GENDER_CHOICES
 
 
 # Create your models here.
+class Subject(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+    
 class Student(models.Model):
     name = models.CharField(max_length=30)
     admno = models.CharField(max_length=30)
     grade = models.CharField(max_length=30)
     gender = models.CharField(choices=GENDER_CHOICES, max_length=1)
     age = models.PositiveSmallIntegerField()
+    subjects = models.ManyToManyField(Subject, blank=True)
 
 
     def __str__(self):
