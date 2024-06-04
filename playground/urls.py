a65c0c5b1d685c81f0d3062c1107e11384f62771
jmumo <jmumo6672@gmail.com>
2024-06-04 15:29:13 +0300

    chatbot dashboard

--- playground/urls.py
+++ playground/urls.py
@@ -7,6 +7,5 @@ urlpatterns = [
     path('signin/', views.signin, name='signin'),
     path('signup/', views.signup, name='signup'),
     path('signout/', views.signout, name='signout'),
-    path('chatbot/', views.chatbot, name='chatbot'),
     path('dashboard/', views.chatbot_dashboard, name='chatbot_dashboard'),
 ]
