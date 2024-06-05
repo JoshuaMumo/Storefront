b5e799889b0d09b6a9b51cf8a9ccf55f4333448a
jmumo <jmumo6672@gmail.com>
2024-06-05 10:41:15 +0300

    updated models

--- playground/models.py
+++ playground/models.py
@@ -20,11 +20,15 @@ class Student(models.Model):
 
     def __str__(self):
         return self.name
+    class Meta:
+        ordering = ['grade']
 
 class Teacher(models.Model):
     name = models.CharField(max_length=100)
     gender = models.CharField(choices=GENDER_CHOICES, max_length=1)
     age = models.PositiveSmallIntegerField()
-
+    grade_assigned = models.CharField(max_length=30, default='Unassigned')
+    subjects = models.ManyToManyField(Subject, blank=True)
+    
     def __str__(self):
         return self.name
