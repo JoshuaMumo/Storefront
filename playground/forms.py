c1fba0302d616743eeaeed9404a17a8cc28f7de2
jmumo <jmumo6672@gmail.com>
2024-06-07 10:21:32 +0300

    added forms

--- playground/forms.py
+++ playground/forms.py
@@ -2,7 +2,7 @@ from django.forms import ModelForm
 from .models import *
 from django.contrib.auth.models import User
 
-class StuentForm(ModelForm):
+class StudentForm(ModelForm):
     class Meta:
         model = Student
         fields = ['name', 'admno', 'password']
